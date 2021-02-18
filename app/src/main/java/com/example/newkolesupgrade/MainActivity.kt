7008e5bf586a04e2ec61dc1fef160120ebd8c8c7
kudaikulov <70584169+kudaikulov@users.noreply.github.com>
2021-02-18 21:28:10 +0600

    new ee

--- app/src/main/java/com/example/newkolesupgrade/MainActivity.kt
+++ app/src/main/java/com/example/newkolesupgrade/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var responseButton: Button
     private lateinit var responseTextView: TextView
 
+//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
